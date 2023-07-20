bfe127eeba4f9ff6fb516424b18c7b8e4ec15ba6
Nkemjika1divine <cndivine@gmail.com>
2023-07-19 22:27:50 -0700

    Simple shell

--- main.c
+++ main.c
@@ -80,15 +80,18 @@ void exec(char *input, char **argv, char **env)
 	pid_t pid;
 	int status, i;
 	char *vec[] = {NULL, NULL};
+	char *tokens[10];
 
 	/*break input intk tokens*/
 	vec[0] = strtok(input, " ");
 	i = 0;
-	while (vec[i] != NULL)
+	while (vec[i] != NULL && i < 10)
 	{
+		tokens[i] = vec[i];
 		i++;
 		vec[i] = strtok(NULL, " ");
 	}
+	tokens[i] = NULL;
 
 	/*create processes*/
 	pid = fork();
@@ -101,7 +104,7 @@ void exec(char *input, char **argv, char **env)
 	}
 	else if (pid == 0)
 	{
-		if (execve(vec[0], vec, env) == -1)
+		if (execve(tokens[0], tokens, env) == -1)
 			printf("%s: %s No such file or directory", argv[0], input);
 	}
 	else
